6afb1d921e10c5838de68b1099c86e596cb37500
Wanwamy <longvu1217@gmail.com>
2024-08-20 12:36:42 +0700

    update css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+import './App.css';
 
 function App() {
   const [data, setData] = useState([]);
@@ -17,7 +18,7 @@ function App() {
     // Axios
     // getData();
 
-    //Fetch API
+    // Using Fetch API
     fetch('/user')
       .then(response => response.json())
       .then(data => {
@@ -32,7 +33,12 @@ function App() {
       <ul>
         {data.map((user, index) => (
           <li key={index}>
-            {user.name} - {user.email}
+            <div><strong>Name </strong> : {user.name}</div>
+            <div><strong>Email</strong> : {user.email}</div>
+            <div><strong>Phone</strong> : {user.phonenumber}</div>
+            <div><strong>Role </strong> : {user.role}</div>
+            <div><strong>Age</strong> : {user.age}</div>
+            <div><strong>Sex</strong> : {user.sex}</div>
           </li>
         ))}
       </ul>
