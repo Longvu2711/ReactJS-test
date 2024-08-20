a50bd70249a282961ad4fe4e22143daef5f15572
Wanwamy <longvu1217@gmail.com>
2024-08-20 10:50:29 +0700

    show user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await Axios.get('http://localhost:8080/user');
+      setData(res.data);
+    } catch (error) {
+      console.error('Error fetching data with Axios:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Axios
+    // getData();
+
+    //Fetch API
+    fetch('/user')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => console.error('Error fetching data with Fetch:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {data.map((user, index) => (
+          <li key={index}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
